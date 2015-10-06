01c87a5e1b156b6b1fab0356c26e1b21827f48a5
cw100 <connordixie@gameviewer.co.uk>
2015-10-06 13:11:05 +0100

    intital

---
+++ pong/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pong
+{
+    class Ball
+    {
+        public int x;
+        public int y;
+       
+        public int Speed;
+        public int yDirection;
+        public int xDirection;
+        public string ballString;
+        public void Update()
+        {
+            x += xDirection;
+            y += yDirection;
+        }
+    }
+}
